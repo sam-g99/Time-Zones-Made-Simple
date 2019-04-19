7db546ae7b5fe1362c14ddda54faa87f728707d4
SamTheDev <46730010+sam-g99@users.noreply.github.com>
2019-04-19 05:45:02 -0400

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,6 @@ export default function Footer() {
   return (
     <div>
     <footer>
-        <p style={{float:'left'}}>Privacy Policy</p>
         <p style={{float:'right'}}>made by <a href="https://github.com/sam-g99" target="_blank">@sam-g99</a></p>
     </footer>
     </div>
