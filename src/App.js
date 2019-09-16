8f9a23a92fbba0130ed2e29696ea4ef6ed1e3f9a
Samuel <46730010+sam-g99@users.noreply.github.com>
2019-09-16 01:15:45 -0400

    removed br

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
       <Router>
       <div className="App">
       <nav>
-      <Link to='/'>Home</Link><br></br>
+      <Link to='/'>Home</Link>
       <Link to='/create/event'>Create Link</Link>
       </nav>
       <Switch>
